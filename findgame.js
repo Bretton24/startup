4db2731d6d589ac77c8d9899cf0477b463397c17
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2023-05-25 13:25:28 -0600

    files created

---
+++ findgame.js
@@ -0,0 +1,6 @@
+function initMap() {
+    var center = { lat: 37.7749, lng: -122.4194 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: center,
+      zoom: 8
+    })}
