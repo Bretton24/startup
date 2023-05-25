9e12791bee085ba7f13f6969740799d350c23830
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2023-05-25 17:43:31 -0600

    created a map using google maps api

--- findgame.js
+++ findgame.js
@@ -1,6 +1,20 @@
-function initMap() {
-    var center = { lat: 37.7749, lng: -122.4194 };
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: center,
-      zoom: 8
-    })}
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of provo
+  const position = { lat: 40.233845, lng: -111.658531 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 10,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+}
+
+initMap();
