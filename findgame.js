1317b0bc0e67e6dbdf38e80fc71070fa179417f0
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2023-05-25 23:33:47 -0600

    added geocode to make the map interactive

--- findgame.js
+++ findgame.js
@@ -4,28 +4,87 @@ userNameElement.innerText = storedUsername;
 
 // Initialize and add the map
 let map;
+let marker = [];
+let geocoder;
+let responseDiv;
+let response;
 
 async function initMap() {
-  // The location of Provo
-  const position = { lat: 40.233845, lng: -111.658531 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: { lat: 40.233845, lng:-111.658531 }
+    });
+    geocoder = new google.maps.Geocoder();
 
-  // The map, centered at Provo
-  map = new Map(document.getElementById("map"), {
-    zoom: 10,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
+    const inputText = document.createElement("input");
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Provo",
-  });
-}
+    inputText.placeholder = "Enter only full addresses";
 
-initMap();
+    const submitButton = document.createElement("input");
+
+    submitButton.type = "button";
+    submitButton.value - "Geocode";
+    submitButton.classList.add("button", "button-primary");
+
+    const clearButton = document.createElement("input");
+
+    clearButton.type = "button";
+    clearButton.value = "Clear";
+    clearButton.classList.add("button", "button-secondary");
+    response = document.createElement("pre");
+    response.id = "response";
+    response.innerText = "";
+    responseDiv = document.createElement("div");
+    responseDiv.id = "response-container";
+    responseDiv.appendChild(response);
+
+    const instructionsElement = document.createElement("p");
+
+    instructionsElement.id = "instructions";
+    instructionsElement.innerHTML =
+      "<strong>Instructions</strong>: Enter an address in the textbox to geocode or click on the map to reverse geocode.";
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputText);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(submitButton);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(clearButton);
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(
+      //instructionsElement
+    );
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(responseDiv);
+    marker = new google.maps.Marker({
+    map,
+    });
+    map.addListener("click", (e) => {
+    geocode({ location: e.latLng });
+    });
+    submitButton.addEventListener("click", () =>
+    geocode({ address: inputText.value })
+    );
+    clearButton.addEventListener("click", () => {
+    clear();
+    });
+    clear();
+    }
+
+    function clear() {
+        marker.setMap(null);
+    }
+
+    function geocode(request) {
+        clear();
+        geocoder
+          .geocode(request)
+          .then((result) => {
+            const { results } = result;
+      
+            map.setCenter(results[0].geometry.location);
+            marker.setPosition(results[0].geometry.location);
+            marker.setMap(map);
+            response.innerText = JSON.stringify(result, null, 2);
+            return results;
+          })
+          .catch((e) => {
+            alert("Geocode was not successful for the following reason: " + e);
+          });
+      }
+      window.initMap = initMap;
+  
