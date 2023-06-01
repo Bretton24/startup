d358148f5ab92beabf998eacc4b9eb7ba1824ddc
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2023-06-01 13:44:28 -0600

    changes made

--- npmtest/index.js
+++
@@ -1,4 +0,0 @@
-const giveMeAJoke = require('give-me-a-joke');
-giveMeAJoke.getRandomJokeOfTheDay((joke) => {
-    console.log(joke);
-})
--- webservicetest/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.resou
-
-app.listen(8080);
