0839dc4fb8e1234c607ed119131e6532e5cefaf7
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2023-06-08 23:08:38 -0600

    created more functions in the database.js file that create get and getuserby token through the database

--- database.js
+++ database.js
@@ -18,6 +18,27 @@ const playerCollection = db.collection('players');
     process.exit(1);
 });
 
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+  
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+
+  async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+  }
 
 
 
