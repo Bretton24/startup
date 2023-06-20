742c99f3d6994dfc64b367ada94ef18c059a6ce4
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2023-06-19 22:08:54 -0600

    finished the app.jsx whihc displays the navigation bar

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className='body bg-whitish text-dark'>
+        <header class="container-fluid">
+            <nav className="navbar fixed-top ">
+                <a className="navbar-brand font" href="#">GamesWFriends<sup>&reg;</sup></a>
+                <menu className="navbar-dark">
+                    <li className="nav-item">
+                        <a className="highlightlink nav-link active font" href="index.html">Home</a>
+                    </li>
+                </menu>
+            </nav>
+        </header>
+
+        <main>App components go here</main>
+
+        <footer className="players">
+            <div className="container-fluid">
+                <span className="text-reset">Brett Ashcraft</span>
+                <a className="text-reset" href="https://github.com/Bretton24/startup">GitHub</a>
+            </div>
+        </footer>
+    </div>
+  ) 
 }
