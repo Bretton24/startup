765a9eb7b7f780c4bf8dcdff78ccb4046d8c9a06
Bretton24 <103625821+Bretton24@users.noreply.github.com>
2023-06-19 23:49:25 -0600

    added the css files for login, gamesplayed, and findgame. Copied over info from original

--- src/gamesplayed/gamesplayed.jsx
+++ src/gamesplayed/gamesplayed.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import './gamesplayed.css';
 
 export function Gamesplayed() {
   return (
     <main className='container-fluid bg-lightgray text-center'>
-      <div>gamesplayed displayed here</div>
+      <h1>Games Played</h1>
+      <div id="quote"></div>
+      <table className="table table-warning table-striped-columns">
+        <thead className="table-dark font">
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Wins</th>
+            <th>Losses</th>
+            <th>Last Game Date</th>
+          </tr>
+        </thead>
+        <tbody id="players">
+        </tbody>
+      </table>
+      <img src="sports.jpg" alt="image of various sports"></img>
     </main>
   );
 }
